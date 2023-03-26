34caa2c6f1f7d0061ac3ee9356cc3439d0797e2c
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.pl>
2023-03-26 19:34:17 +0200

    Initial commit

---
+++ src/main/java/com/example/workshops/WorkshopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopsApplication.class, args);
+	}
+
+}
